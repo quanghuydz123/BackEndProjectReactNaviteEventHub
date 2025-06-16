90fa730fef247bd4bdea084565a8dbced8b8ce6a
Nguyen Quang Huy <huy.q.nguyen@tgl-sol.com>
2025-06-16 23:35:01 +0700

    fix bug follow

--- src/controller/followController.js
+++ src/controller/followController.js
@@ -199,7 +199,7 @@ const updateFollowUserOther = asyncHandle(async (req, res) => {
         await NotificationModel.create({
             senderID: idUser,
             recipientId: idUserOther,
-            type: 'allowFollow',
+            type: 'follow',
             content: `vừa mới theo dõi bạn !!!`,
             status: 'unanswered',
             isRead: false
